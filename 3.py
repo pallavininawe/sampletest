2a82798aaa23f4e38f4e59b50f999890d5c5f0a1
pallavi_ninawe <pallavi.ninawe@quantiphi.com>
2017-07-10 17:22:44 +0530

    third commit for file 3.py

--- 3.py
+++ 3.py
@@ -2,10 +2,10 @@ import sys
 a=(sys.argv[1])
 b=len(a)
 print(b)
-for i in range(0,b):
-	if (a[i]!=a[b-i-1]):
+for j in range(0,b):
+	if (a[j]!=a[b-j-1]):
 		 break
-if i < int(b/2 + 1):
+if j < int(b/2 + 1):
 	print ("not a palindrome")
 else:
 	print ("palindrome")

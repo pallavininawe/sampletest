ddb9fb8853dfced47c8a50a9210026f0495cbab2
pallavi_ninawe <pallavi.ninawe@quantiphi.com>
2017-07-11 17:08:09 +0530

    updating VowelOrConsonent.py

--- Day2/VowelOrConsonent.py
+++ Day2/VowelOrConsonent.py
@@ -1,6 +1,6 @@
 import string
-a=raw_input("Enter a letter : ")
-upper=a.upper()
+b=raw_input("Enter a letter : ")
+upper=b.upper()
 if ( upper == 'A' or upper == 'E' or upper== 'I' or upper== 'O' or upper== 'U'):
 	print("Vowel")
 else:

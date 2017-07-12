70f55d69a45045748eec16a8812a19cc54b5f642
pallavi_ninawe <pallavi.ninawe@quantiphi.com>
2017-07-12 15:42:01 +0530

    Push Day1

---
+++ Day1/3.py
@@ -0,0 +1,11 @@
+import sys
+a=(sys.argv[1])
+b=len(a)
+print(b)
+for j in range(0,b):
+	if (a[j]!=a[b-j-1]):
+		 break
+if j < int(b/2 + 1):
+	print ("not a palindrome")
+else:
+	print ("palindrome")
